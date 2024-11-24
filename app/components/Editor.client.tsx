8baba575edd8cf6aed812c5e785b1c1e842d26a7
Yomesh Gupta <yomesh.gupta@gmail.com>
2024-11-24 17:02:04 +0530

    Added Editor

---
+++ app/components/Editor.client.tsx
@@ -0,0 +1,22 @@
+import CodeMirror from "@uiw/react-codemirror";
+import { javascript } from "@codemirror/lang-javascript";
+
+const Editor = ({
+  code,
+  onChange,
+}: {
+  code: string;
+  onChange(updatedValue: string): void;
+}) => {
+  return (
+    <CodeMirror
+      value={code}
+      minHeight="100vh"
+      extensions={[javascript({ jsx: true })]}
+      onChange={onChange}
+      theme="dark"
+    />
+  );
+};
+
+export default Editor;
