d822fa2d567d681ddf3bd833e59793c3d054eaa4
Yomesh Gupta <yomesh.gupta@gmail.com>
2024-11-27 19:30:50 +0530

    Added generate review button

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,5 +1,6 @@
 import type { MetaFunction } from "@remix-run/node";
 import { useState } from "react";
+import classnames from "classnames";
 
 import Editor from "~/components/Editor.client";
 import Review from "~/components/Review.client";
@@ -14,13 +15,20 @@ export const meta: MetaFunction = () => {
 export default function Index() {
   const [code, setCode] = useState("");
   const [review, setReview] = useState("");
+  const [state, setState] = useState<"idle" | "loading" | "generated">("idle");
 
   const onChange = (updatedValue: string) => setCode(updatedValue);
 
   return (
     <div className="flex h-screen items-center justify-center">
       <div className="flex flex-row h-full w-full">
-        <div className="h-full w-6/12">
+        <div className="h-full w-6/12 relative">
+          <button
+            className="w-max absolute bottom-3 right-3 z-50 bg-green-500 p-2 rounded hover:bg-green-700 active:translate-y-1 disabled:opacity-75  disabled:pointer-events-none disabled:cursor-not-allowed"
+            disabled={state === "loading"}
+          >
+            Generate Review
+          </button>
           <Editor code={code} onChange={onChange} />
         </div>
         <div className="h-full w-6/12">
